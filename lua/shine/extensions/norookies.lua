3b8ba8570f2de62e1532bb26107dc275382134af
BrightPaul <sebi4d@gmx.de>
2014-01-15 16:40:21 +0100

    NoRookies now uses also ns2stats

--- lua/shine/extensions/norookies.lua
+++ lua/shine/extensions/norookies.lua
@@ -8,9 +8,9 @@ local Plugin = {}
 
 local Notify = Shared.Message
 local StringFormat = string.format
-local HTTPRequest = Shine.TimedHTTPRequest
+local HTTPRequest = Shared.SendHTTPRequest
 
-Plugin.Version = "1.0"
+Plugin.Version = "1.5"
 Plugin.DefaultState = false
 
 Plugin.HasConfig = true
@@ -18,6 +18,7 @@ Plugin.HasConfig = true
 Plugin.ConfigName = "norookies.json"
 Plugin.DefaultConfig =
 {
+    Ns2StatsUrl = "http://ns2stats.com",
     MinPlayer = 0,
     DisableAfterRoundtime = 0,
     MinPlaytime = 8,
@@ -36,27 +37,66 @@ Shine.Hook.SetupClassHook( "CommandStructure", "OnUse", "CheckComLogin","ActiveP
 
 local Enabled = true
 local HiveData = {}
+local Ns2StatsData = {}
+local PlayTime = {}
 
 function Plugin:ClientConnect( Client )
-    if not Shine:IsValidClient( Client ) then return end
+    if not Shine:IsValidClient( Client ) or Shine:HasAccess(Client, "sh_ignorestatus" ) then return end
     
     local steamid = Client:GetUserId()
     if not steamid or steamid <= 0 then return end   
     
-    if HiveData[steamid] then return end
+    if PlayTime[steamid] then return end
+    
     if not self:TimerExists(StringFormat("Wait_%s", steamid)) then
         self:CreateTimer(StringFormat("Wait_%s", steamid), self.Config.HTTPMaxWaitTime, 1, function()
-            HiveData[steamid] = 0
+            PlayTime[steamid] = -1
         end)
     end
     
-    local URL = StringFormat("http://sabot.herokuapp.com/api/get/playerData/%s",steamid)    
-    HTTPRequest( URL, "GET", function(response)
-        HiveData[steamid] = json.decode(response) or 0
-        self:DestroyTimer(StringFormat("Wait_%s", steamid))
-    end,function()
-        self:ClientConnect( Client ) 
-    end)
+    if not Ns2StatsData[steamid] then
+        local SURL = self.Config.Ns2StatsUrl .. "/api/player?ns2_id=" .. steamid
+        HTTPRequest( SURL, "GET", function(response)
+            Ns2StatsData[steamid] = json.decode(response) and json.decode(response)[1] or 0
+            if HiveData[steamid] then
+                if HiveData[steamid] == 0 then
+                    if Ns2StatsData[steamid] == 0 then
+                        PlayTime[steamid] = -1
+                    else
+                        PlayTime[steamid] = Ns2StatsData[steamid].time_played and tonumber(Ns2StatsData[steamid].time_played) or 0    
+                    end                    
+                else
+                    local splaytime = Ns2StatsData[steamid].time_played and tonumber(Ns2StatsData[steamid].time_played) or 0
+                    local hplaytime = HiveData[steamid].playTime and tonumber(HiveData[steamid].playTime) or 0
+                    PlayTime[steamid] = splaytime > hplaytime and splaytime or hplaytime
+                end
+                self:DestroyTimer(StringFormat("Wait_%s", steamid))
+            end
+        end)
+    end
+    
+    if not HiveData[steamid] then
+        local HURL = StringFormat("http://sabot.herokuapp.com/api/get/playerData/%s",steamid)    
+        HTTPRequest( HURL, "GET", function(response)
+            HiveData[steamid] = json.decode(response) or 0
+            if Ns2StatsData[steamid] then
+                if Ns2StatsData[steamid] == 0 then
+                    if HiveData[steamid] == 0 then
+                        PlayTime[steamid] = -1
+                    else
+                        PlayTime[steamid] = HiveData[steamid].playTime and tonumber(HiveData[steamid].playTime) or 0   
+                    end                    
+                else
+                    local splaytime = Ns2StatsData[steamid].time_played and tonumber(Ns2StatsData[steamid].time_played) or 0
+                    local hplaytime = HiveData[steamid].playTime and tonumber(HiveData[steamid].playTime) or 0
+                    PlayTime[steamid] = splaytime > hplaytime and splaytime or hplaytime
+                end
+                self:DestroyTimer(StringFormat("Wait_%s", steamid))
+            end
+        end)
+    end
+    
+    self:SimpleTimer(5,function() self:ClientConnect( Client ) end)
 end
 
 function Plugin:SetGameState( Gamerules, NewState, OldState )
@@ -80,11 +120,9 @@ function Plugin:CheckComLogin( Chair, Player )
     if not steamid or steamid <= 0 then return end
     
     if self:TimerExists(StringFormat("Wait_%s", steamid)) then self:Notify(Player, self.Config.WaitMessage) return false end
-    local pdata = HiveData[steamid]
-    if not pdata or pdata == 0 then return end --hive timeout
         
-    local playtime = pdata.playTime and tonumber(pdata.playTime) or 0   
-    if playtime < self.Config.MinPlaytime * 3600 then
+    local playtime = PlayTime[steamid]
+    if playtime >= 0 and playtime < self.Config.MinPlaytime * 3600 then
         self:Notify(Player, self.Config.BlockMessage)
         self:Kick(Player)
         return false
@@ -96,7 +134,7 @@ function Plugin:Notify( Player, Message )
 end
 
 function Plugin:JoinTeam( Gamerules, Player, NewTeam, Force, ShineForce )    
-    if not Enabled or #Shine.GetAllPlayers() < self.Config.MinPlayer then return end
+    if not Enabled or not self.Config.BlockTeams or #Shine.GetAllPlayers() < self.Config.MinPlayer then return end
     
     local client = Player:GetClient()
     if not Shine:IsValidClient(client) or Shine:HasAccess(client, "sh_ignorestatus" ) then return end
@@ -105,11 +143,9 @@ function Plugin:JoinTeam( Gamerules, Player, NewTeam, Force, ShineForce )
     if not steamid or steamid <= 0 then return end
     
     if self:TimerExists(StringFormat("Wait_%s",steamid)) then self:Notify(Player, self.Config.WaitMessage) return false end
-    local pdata = HiveData[steamid]
-    if not pdata or pdata == 0 then return end --hive timeout
-        
-    local playtime = pdata.playTime and tonumber(pdata.playTime) or 0
-    if self.Config.BlockTeams and playtime < self.Config.MinPlaytime * 3600 then
+    
+    local playtime = PlayTime[steamid]
+    if playtime >= 0 and playtime < self.Config.MinPlaytime * 3600 then
         self:Notify(Player, self.Config.BlockMessage)
         self:Kick(Player)
         return false 
