e0dfb35b5d600aae22ea0cb61fd7a0ece825caf7
GhoulofGSG9 <ghoulofgsg9@gmail.com>
2014-09-28 17:12:18 +0200

    Using now Shine:GetClient

--- lua/shine/extensions/captains/server.lua
+++ lua/shine/extensions/captains/server.lua
@@ -8,7 +8,6 @@ local ToNumber = tonumber
 local TableInsert = table.insert
 local TableRemove = table.remove
 local Random = math.random
-local GetClientByNS2ID = Shine.GetClientByNS2ID
 local SetupClassHook = Shine.Hook.SetupClassHook
 local GetOwner = Server.GetOwner
 
@@ -202,7 +201,7 @@ function Plugin:SetCaptain( SteamId, TeamNumber )
 	self.Teams[ TeamNumber ].Captain = SteamId
 	self.Teams[ TeamNumber ].Players[ SteamId ] = true
 	
-	local Client = GetClientByNS2ID( SteamId )
+	local Client = Shine:GetClient( SteamId )
 	
 	if not Client then return end
 	local Player = Client:GetControllingPlayer()
@@ -225,7 +224,7 @@ function Plugin:RemoveCaptain( TeamNumber, SetCall )
 	self.Teams[ TeamNumber ].Players[ SteamId ] = false
 	self.Teams[ TeamNumber ].Captain = nil	
 
-	local Client = GetClientByNS2ID( SteamId )
+	local Client = Shine:GetClient( SteamId )
 	local Player = Client:GetControllingPlayer()
 	
 	self:Notify( nil, "%s is now not any longer the Captain of Team %s", true, Player:GetName(), TeamNumber )
@@ -277,9 +276,9 @@ function Plugin:PostJoinTeam( Gamerules, Player, OldTeam, NewTeam, Force, ShineF
 			local OldVoteId = Vote:GetOptionName( Vote:GetVote( Client ) )
 			if OldVoteId then
 				Vote:RemoveVote( Client )
-				local OldVoteClient = GetClientByNS2ID( OldVoteId )
+				local OldVoteClient = Shine:GetClient( OldVoteId )
 				local OldVotePlayer = OldVoteClient:GetControllingPlayer()
-				self:SendPlayerData( nil, Player )
+				self:SendPlayerData( nil, OldVotePlayer )
 			end
 		end
 		
@@ -488,7 +487,7 @@ function Plugin:EndGame( Gamerules, WinningTeam )
 	
 	local AllCaptains = true
 	for i = 1, 2 do
-		local Client = GetClientByNS2ID( self.Teams[ i ].Captain )
+		local Client = Shine:GetClient( self.Teams[ i ].Captain )
 		if not Client then
 			AllCaptains = false
 			break
@@ -579,7 +578,7 @@ function Plugin:CreateCommands()
 			local OldVoteId = Vote:GetOptionName( Vote:GetVote( Client ) )
 			if OldVoteId == TargetId then return end --revote 
 			if OldVoteId then
-				local OldVoteClient = GetClientByNS2ID( OldVoteId )
+				local OldVoteClient = Shine:GetClient( OldVoteId )
 				local OldVotePlayer = OldVoteClient:GetControllingPlayer()
 				self:SendPlayerData( nil, OldVotePlayer )
 			end
